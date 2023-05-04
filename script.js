b2b2eeddea6834a812700028ae41e916eacf1fd5
Peter Montgomery <montyman1218@gmail.com>
2023-05-04 16:02:24 -0400

    init garment array

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
+const garmentArray = [
+    {
+        name: 'ramones',
+        img: 'assets/rick/ramones.png',
+    },
+    {
+        name: 'shorts',
+        img: 'assets/rick/shorts.png',
+    }
+]
+
+//trick to sort array randomly!
+garmentArray.sort(() => 0.5 - Math.random())
+const gridDisplay = document.querySelector('#grid')
+function createWardrobe(){
+    for (let i = 0; i < garmentArray.length; i++){
+        const garm = document.createElement('img')
+        garm.setAttribute('src', garmentArray[i].img)
+        garm.setAttribute('data-id', i)
+        //garm.addEventListener('click', selectGarm)
+        gridDisplay.append(garm)
+    }
+}
+createWardrobe()
+
+
+var character = document.getElementById("scene");
+var garment = document.getElementById("garment");
 var counter=0;
 function puton(){
     if(character.classList == "animate"){return}
@@ -8,16 +34,16 @@ function puton(){
         character.classList.remove("animate");
     },300);
 }
-var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
-    }
-}, 10);
+// var checkDead = setInterval(function() {
+//     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+//     let blockLeft = parseInt(window.getComputedStyle(garment).getPropertyValue("left"));
+//     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+//         block.style.animation = "none";
+//         alert("Game Over. score: "+Math.floor(counter/100));
+//         counter=0;
+//         block.style.animation = "block 3s infinite linear";
+//     }else{
+//         counter++;
+//         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+//     }
+// }, 10);
